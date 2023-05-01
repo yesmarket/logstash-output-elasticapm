57e5e85532c891b60d87b0337b551002933a28c8
Ryan Bartsch <rbartsch@adam.com.au>
2023-05-01 11:51:44 +0930

    .

--- lib/logstash/outputs/elasticapm.rb
+++ lib/logstash/outputs/elasticapm.rb
@@ -1,16 +1,49 @@
 # encoding: utf-8
 require "logstash/outputs/base"
+require "logstash/namespace"
+require "rest-client"
+require "json"
+require "jsonpath"
+require "base64"
 
 # An elasticapm output that does nothing.
 class LogStash::Outputs::Elasticapm < LogStash::Outputs::Base
   config_name "elasticapm"
 
+  config :apm_server_uri, :validate => :string, :requried => true
+
+  config :api_key, :validate => :password, :required => true
+
+  config :base64_encoded_proto_field, :validate => :string, :default => "$..data"
+
   public
   def register
+    @uri = "#{@apm_server_uri}/v1/traces"
   end # def register
 
   public
-  def receive(event)
-    return "Event received"
+  def multi_receive(events)
+    begin
+      json = event.to_jsonx
+      #puts jsonEvent
+      headers = {}
+      headers['Content-Type'] = 'application/x-protobuf'
+      headers['Authorization'] = "ApiKey #{@api_key}"
+      #puts path
+      results = JsonPath.new(@base64_encoded_proto_field).on(json)
+      #puts results
+      results.each do |item|
+        #puts item
+        requestBody = Base64.decode64(item)
+        response = RestClient.post(@uri, requestBody, headers)
+        unless response.code == 200
+          #puts "Elastic APM request failure: error code: #{response.code}, data=>#{event}"
+          @logger.error("Elastic APM request failure: error code: #{response.code}, data=>#{event}")
+        end
+      end
+    rescue Exception => ex
+      #puts "Exception occurred in posting to Elastic APM Server: '#{ex}', data=>#{event}"
+      @logger.error("Exception occurred in posting to Elastic APM Server: '#{ex}', data=>#{event}")
+    end
   end # def event
 end # class LogStash::Outputs::Elasticapm
--- logstash-output-elasticapm.gemspec
+++ logstash-output-elasticapm.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name          = 'logstash-output-elasticapm'
-  s.version       = '0.1.0'
+  s.version       = '0.2.0'
   s.licenses      = ['Apache-2.0']
-  s.summary       = 'Logstash Output Plugin for Elasticapm'
-  s.description   = 'TODO: Write a longer description or delete this line.'
-  s.homepage      = 'TODO: Put your plugin''s website or public repo URL here.'
-  s.authors       = ['your_username']
-  s.email         = 'your_username@example.com'
+  s.summary       = 'Logstash output plugin for Elastic APM'
+  s.description   = 'Logstash output plugin to send traces and metrics to Elastic APM Server'
+  s.homepage      = 'https://github.com/yesmarket/logstash-output-elasticapm'
+  s.authors       = ['Ryan Bartsch']
+  s.email         = 'rbartsch@yandex.com'
   s.require_paths = ['lib']
 
   # Files
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
   s.metadata = { "logstash_plugin" => "true", "logstash_group" => "output" }
 
   # Gem dependencies
+  s.add_runtime_dependency "rest-client", "~> 2.1"
   s.add_runtime_dependency "logstash-core-plugin-api", "~> 2.0"
   s.add_runtime_dependency "logstash-codec-plain"
   s.add_development_dependency "logstash-devutils"
