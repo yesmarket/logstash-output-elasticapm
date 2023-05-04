5780a2eb3778b63c7654467a57cb70ca69f0ca00
Ryan Bartsch <rbartsch@adam.com.au>
2023-05-05 09:09:20 +0930

    working

--- lib/logstash/outputs/elasticapm.rb
+++ lib/logstash/outputs/elasticapm.rb
@@ -12,37 +12,44 @@ class LogStash::Outputs::Elasticapm < LogStash::Outputs::Base
 
   config :apm_server_uri, :validate => :string, :requried => true
 
-  config :api_key, :validate => :password, :required => true
+  config :otlp_type, :validate => :string, :requried => true
+
+  config :api_key, :validate => :string, :required => true
 
   config :base64_encoded_proto_field, :validate => :string, :default => "$..data"
 
   public
   def register
-    @uri = "#{@apm_server_uri}/v1/traces"
+   @uri = "#{@apm_server_uri}/v1/#{@otlp_type}"
   end # def register
 
   public
-  def multi_receive(events)
+  def receive(event)
     begin
-      json = event.to_jsonx
-      #puts jsonEvent
+      json = event.to_json
+      #puts json
       headers = {}
       headers['Content-Type'] = 'application/x-protobuf'
       headers['Authorization'] = "ApiKey #{@api_key}"
-      #puts path
+      #puts @uri
+      #puts headers
       results = JsonPath.new(@base64_encoded_proto_field).on(json)
       #puts results
       results.each do |item|
         #puts item
         requestBody = Base64.decode64(item)
+        #puts requestBody
         response = RestClient.post(@uri, requestBody, headers)
+        #puts response.code
+        #puts response.body
+        #puts response.headers
         unless response.code == 200
-          #puts "Elastic APM request failure: error code: #{response.code}, data=>#{event}"
+          puts "Elastic APM request failure: error code: #{response.code}, data=>#{event}"
           @logger.error("Elastic APM request failure: error code: #{response.code}, data=>#{event}")
         end
       end
     rescue Exception => ex
-      #puts "Exception occurred in posting to Elastic APM Server: '#{ex}', data=>#{event}"
+      puts "Exception occurred in posting to Elastic APM Server: '#{ex}', data=>#{event}"
       @logger.error("Exception occurred in posting to Elastic APM Server: '#{ex}', data=>#{event}")
     end
   end # def event
--- logstash-output-elasticapm.gemspec
+++ logstash-output-elasticapm.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |s|
 
   # Gem dependencies
   s.add_runtime_dependency "rest-client", "~> 2.1"
+  s.add_runtime_dependency "jsonpath", "~> 1.1"
   s.add_runtime_dependency "logstash-core-plugin-api", "~> 2.0"
   s.add_runtime_dependency "logstash-codec-plain"
   s.add_development_dependency "logstash-devutils"
